4057fb8111a353f9770bd928563d09844e254a34
GrinZero <774933704@qq.com>
2024-06-15 01:03:02 +0800

    chore: add husky and prettierrc

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -1,34 +1,34 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['only-warn'],
   globals: {
     React: true,
-    JSX: true,
+    JSX: true
   },
   env: {
-    node: true,
+    node: true
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
-        project,
-      },
-    },
+        project
+      }
+    }
   },
   ignorePatterns: [
     // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
+    '.*.js',
+    'node_modules/',
+    'dist/'
   ],
   overrides: [
     {
-      files: ["*.js?(x)", "*.ts?(x)"],
-    },
-  ],
-};
+      files: ['*.js?(x)', '*.ts?(x)']
+    }
+  ]
+}
